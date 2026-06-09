30ddc0c6b392c3a5e1ebf7cc79ae70470b1bacaf
wesleyromanely-lang <wesley.romanely@gmail.com>
2026-06-08 21:36:38 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,10 @@
-// Configuração Firebase Toda Bella Studio
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  push
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAstm5bEDdHWCKSVPhs4cfddCT-SYNqjZA",
@@ -7,6 +13,10 @@ const firebaseConfig = {
   projectId: "todabelastudio-ea3ac",
   storageBucket: "todabelastudio-ea3ac.firebasestorage.app",
   messagingSenderId: "289555596882",
-  appId: "1:289555596882:web:32519783d49afacb28c18b",
-  measurementId: "G-9MW1G76D3B"
+  appId: "1:289555596882:web:32519783d49afacb28c18b"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export { ref, push };
