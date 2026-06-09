cd5aabc255282a2eb601bff87e5ede92096247b8
wesleyromanely-lang <wesley.romanely@gmail.com>
2026-06-08 21:16:54 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+// Configuração Firebase Toda Bella Studio
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAstm5bEDdHWCKSVPhs4cfddCT-SYNqjZA",
+  authDomain: "todabelastudio-ea3ac.firebaseapp.com",
+  databaseURL: "https://todabelastudio-ea3ac-default-rtdb.firebaseio.com",
+  projectId: "todabelastudio-ea3ac",
+  storageBucket: "todabelastudio-ea3ac.firebasestorage.app",
+  messagingSenderId: "289555596882",
+  appId: "1:289555596882:web:32519783d49afacb28c18b",
+  measurementId: "G-9MW1G76D3B"
+};
