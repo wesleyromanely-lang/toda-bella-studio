c98ca86a57b7442071a70dd810a6281730c55972
wesleyromanely-lang <wesley.romanely@gmail.com>
2026-06-16 22:35:03 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,32 +1,72 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { 
+initializeApp 
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 
 import {
-  getDatabase,
-  ref,
-  push,
-  get,
-  child,
-  remove
+
+getDatabase,
+ref,
+push,
+get,
+child,
+remove
+
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAstm5bEDdHWCKSVPhs4cfddCT-SYNqjZA",
-  authDomain: "todabelastudio-ea3ac.firebaseapp.com",
-  databaseURL: "https://todabelastudio-ea3ac-default-rtdb.firebaseio.com",
-  projectId: "todabelastudio-ea3ac",
-  storageBucket: "todabelastudio-ea3ac.firebasestorage.app",
-  messagingSenderId: "289555596882",
-  appId: "1:289555596882:web:32519783d49afacb28c18b"
+
+
+apiKey: "AIzaSyAstm5bEDdHWCKSVphs4cfddCT-SYNqjZA",
+
+authDomain: "todabelastudio-ea3ac.firebaseapp.com",
+
+databaseURL: "https://todabelastudio-ea3ac-default-rtdb.firebaseio.com",
+
+projectId: "todabelastudio-ea3ac",
+
+storageBucket: "todabelastudio-ea3ac.firebasestorage.app",
+
+messagingSenderId: "289555596882",
+
+appId: "1:289555596882:web:32519783d49afacb28c18b"
+
+
 };
+
+
+
+
+
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
+
+
+
+
+const db = getDatabase(app);
+
+
+
+
+
 
 export {
-  ref,
-  push,
-  get,
-  child,
-  remove
+
+db,
+
+ref,
+
+push,
+
+get,
+
+child,
+
+remove
+
 };
